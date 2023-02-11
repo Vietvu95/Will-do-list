d381843cfeb3a3e44ee01820f802e59518b148f7
vietvu <viet.vu.95@hotmail.com>
2023-02-11 15:18:19 +1100

    first commit-

---
+++ app/controllers/todos_controller.rb
@@ -0,0 +1,49 @@
+class TodosController < ApplicationController
+  before_action :set_todo, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @todos = Todo.all
+  end
+
+  def show
+  end
+
+  def new
+    @todo = Todo.new
+  end
+
+  def edit
+  end
+
+  def create
+    @todo = Todo.new(todo_params)
+
+    if @todo.save
+      redirect_to @todo, notice: 'Task was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @todo.update(todo_params)
+      redirect_to @todo, notice: 'Task was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @todo.destroy
+    redirect_to todos_url, notice: 'Task was successfully destroyed.'
+  end
+
+  private
+    def set_todo
+      @todo = Todo.find(params[:id])
+    end
+
+    def todo_params
+      params.require(:todo).permit(:title, :completed, :description)
+    end
+end
